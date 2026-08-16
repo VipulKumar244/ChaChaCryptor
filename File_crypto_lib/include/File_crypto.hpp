e8122ced78f6053d21cd492bea423982bd85bf61
Vipul Kumar <vipulk1402@gmail.com>
2026-08-16 17:30:39 +0530

    File cryption utility lib cmake configured

---
+++ File_crypto_lib/include/File_crypto.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+
+
+#include<algorithm>
+#include<cstdint>
+#include<cstring>
+#include<expected>
+#include<filesystem>
+#include<fstream>
+#include<span>
+#include<string>
+#include<vector>
+
+#include<KDF.hpp>
+#include<Thread_Pool.hpp>
+
